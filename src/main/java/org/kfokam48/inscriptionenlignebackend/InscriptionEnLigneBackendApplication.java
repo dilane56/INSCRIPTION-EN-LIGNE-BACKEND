e1826d2b87e6edf737263bca72ef87c3270ff599
dilane56 <kamgaingfotso@gmail.com>
2025-06-14 09:49:09 +0100

    first commit

---
+++ src/main/java/org/kfokam48/inscriptionenlignebackend/InscriptionEnLigneBackendApplication.java
@@ -0,0 +1,13 @@
+package org.kfokam48.inscriptionenlignebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscriptionEnLigneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InscriptionEnLigneBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kfokam48/inscriptionenlignebackend/InscriptionEnLigneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kfokam48.inscriptionenlignebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscriptionEnLigneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
